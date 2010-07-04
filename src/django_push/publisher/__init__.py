0c1d333d1f7c9ab7f91b529203764c8021bf9458
Bruno Renié <buburno@gmail.com>
2010-07-04 15:56:16 +0100

    Moved the ping_hub function and added the PUSH_HUB setting to it

---
+++ src/django_push/publisher/__init__.py
@@ -0,0 +1,22 @@
+import urllib
+import urllib2
+
+from django.conf import settings
+
+
+def ping_hub(feed_url, hub_url=None):
+    if hub_url is None:
+        hub_url = getattr(settings, 'PUSH_HUB')
+        if hub_url is None:
+            return
+    params = {
+        'hub.mode': 'publish',
+        'hub.url': feed_url,
+    }
+    data = urllib.urlencode(params)
+    try:
+        response = urllib2.urlopen(hub_url, data)
+    except urllib2.HTTPError, e:
+        if hasattr(e, 'code') and e.code == 204:
+            continue
+        raise

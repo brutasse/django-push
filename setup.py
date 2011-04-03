7ebbb23a0acc74750763205823b62073ec76c73f
Bruno Renié <buburno@gmail.com>
2011-04-03 12:04:24 +0200

    Reworked le setup.py, now using distutils instead of setuptools

--- setup.py
+++ setup.py
@@ -1,31 +1,32 @@
 # -*- coding: utf-8 -*-
 import os
-from setuptools import setup, find_packages
+from distutils.core import setup
+from setuptools import find_packages
 
 def read(file_name):
     return open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
 setup(
-    name = 'django-push',
-    version = '0.3',
-    url = 'http://github.com/brutasse/django-push',
-    license = 'BSD',
-    description = 'PubSubHubbub (PuSH) support for Django',
-    long_description = read('README.rst'),
-
-    author = 'Bruno Renié',
-    author_email = 'bruno@renie.fr',
-
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-
-    install_requires = ['setuptools'],
-
+    name='django-push',
+    version='0.3',
+    author='Bruno Renié',
+    author_email='bruno@renie.fr',
+    url='http://github.com/brutasse/django-push',
+    license='BSD',
+    description='PubSubHubbub (PuSH) support for Django',
+    long_description=read('README.rst'),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'feedparser'
+    ],
     classifiers = [
         'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
     ],
 )

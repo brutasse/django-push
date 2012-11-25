61c13db6cfaf6bcf5ec73bcaaac6b012fb22df55
Bruno Renié <buburno@gmail.com>
2012-11-25 17:15:15 +0100

    requests in install_requires

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'feedparser'
+        'requests',
     ],
     classifiers = [
         'Development Status :: 4 - Beta',

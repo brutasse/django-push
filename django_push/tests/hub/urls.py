6a86025d6154f374467cc9a76cb8a14b18792e9b
Bruno Renié <buburno@gmail.com>
2013-06-22 16:07:26 +0200

    Removed useless files

--- django_push/tests/hub/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-
-urlpatterns = patterns('hub.views',
-    url(r'^$', 'ping', name='ping'),
-)
--- django_push/tests/pub/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-from django_push.tests.pub.feeds import EntryFeed, CustomHubFeed
-
-
-urlpatterns = patterns('',
-    url(r'^feed/$', EntryFeed()),
-    url(r'^customfeed/$', CustomHubFeed()),
-)

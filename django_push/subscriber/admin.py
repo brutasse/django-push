0dbcee87cd3c40f4321f6c08975dbfd3df964e2f
Bruno Renié <buburno@gmail.com>
2013-06-23 20:58:56 +0200

    Added a custom filter for getting expired subscriptions

--- django_push/subscriber/admin.py
+++ django_push/subscriber/admin.py
@@ -1,13 +1,31 @@
 from django.contrib import admin
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _, ungettext
 
 from django_push.subscriber.models import Subscription
 
 
+class ExpirationFilter(admin.SimpleListFilter):
+    title = _('Expired')
+    parameter_name = 'expired'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('true', _('Yes')),
+            ('false', _('No')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'true':
+            return queryset.filter(lease_expiration__lte=timezone.now())
+        if self.value() == 'false':
+            return queryset.filter(lease_expiration__gte=timezone.now())
+
+
 class SubscriptionAmin(admin.ModelAdmin):
     list_display = ('truncated_topic', 'hub', 'verified', 'has_expired',
                     'lease_expiration')
-    list_filter = ('verified', 'hub')
+    list_filter = ('verified', ExpirationFilter, 'hub')
     search_fields = ('topic', 'hub')
     actions = ['renew', 'unsubscribe']
 
